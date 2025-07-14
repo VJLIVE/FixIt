bfee1a47eb608cb2911e1c36c5829130d0f01ed6
Vijay Guttula <vijayguttula001@gmail.com>
2025-07-14 20:26:26 +0530

    Init vite react js with tailwind css

---
+++ Admin Panel/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
